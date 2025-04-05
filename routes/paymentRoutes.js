c09ccaa9d90eec4f6f4949b49f5799abffab7dee
VIVIAN BASSEY <vivianbasseya@gmail.com>
2025-04-05 07:09:22 +0100

    Fix: added missing packages and fixed env issues

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { initiateCardBinding } = require('../controllers/paymentController');
-
 router.post('/initiate-card-binding', initiateCardBinding);
 
 module.exports = router;
