3882647a03be7fc1348657bec64955738a1ceca3
VIVIAN BASSEY <vivianbasseya@gmail.com>
2025-04-11 06:38:06 +0100

    Added Flutterwave secret key for identity verification

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -29,7 +29,7 @@ router.post(
             if (user) return res.status(400).json({ message: "User already exists" });
             const salt = await bcrypt.genSalt(10);
             const hashedPassword = await bcrypt.hash(password, salt);
-            user = new User({ Surname, Firstname, Middlename, email, password: hashedPassword, Phonenumber,Location, nin });
+            user = new User({ Surname, Firstname, Middlename, email, password: hashedPassword, nin });
             await user.save();
             const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
             res.status(201).json({ message: "User registered successfully", token });
