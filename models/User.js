d6f72003e4fd520869a26118b461ab137da0cc32
VIVIAN BASSEY <vivianbasseya@gmail.com>
2025-04-11 12:13:18 +0100

    Added Flutterwave secret key for identity verification

--- models/User.js
+++ models/User.js
@@ -7,7 +7,7 @@ const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     Phonenumber : { type: String, required: true },
-     /*Location : { type: String, required: true },*/
+    Location : { type: String, required: true },
     nin: { type: String, unique: true }, // For KYC verification
     creditAccountLinked: { type: Boolean, default: false },
     debitCardLinked: { type: Boolean, default: false },
