67c55a4909ca8290a528d1064281023f7362f84b
VIVIAN BASSEY <vivianbasseya@gmail.com>
2025-04-28 08:41:23 +0100

    Added Flutterwave secret key for identity verification

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -33,7 +33,7 @@ router.post(
             await user.save();
             const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
             // i just added an user,_id in the return data
-            res.status(201).json({ message: "User registered successfully", token, user._id });
+            res.status(201).json({ message: "User registered successfully", token, "user id":user._id });
         } catch (error) {
             res.status(500).json({ message: "Server error", error: error.message });
         }
